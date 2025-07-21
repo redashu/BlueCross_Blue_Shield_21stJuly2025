9e91a626bfb496b3a74cc9ccc53f44f5d39ad671
Ashutoshh <ashutoshh@linux.com>
2025-07-22 00:14:18 +0530

    updates terraform

--- day1/az-provider.tf
+++ day1/az-provider.tf
@@ -38,3 +38,43 @@ resource "azurerm_subnet" "ashu-subnet" {
   address_prefixes = ["172.16.1.0/24"] 
   depends_on = [azurerm_virtual_network.ashu-example]
 }
+
+# creating azure network interface with private ip access
+
+resource "azurerm_network_interface" "main" {
+  name                = "ashu-nic"
+  location            = azurerm_resource_group.ashu-group.location
+  resource_group_name = azurerm_resource_group.ashu-group.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.ashu-subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "ashu-example-machine"
+  resource_group_name = azurerm_resource_group.ashu-group.name
+  location            = azurerm_resource_group.ashu-group.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "Redhat@12345"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}

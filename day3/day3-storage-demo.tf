1736e109587504b3360faf68869dee1543a374d9
Ashutoshh <ashutoshh@linux.com>
2025-07-23 19:31:02 +0530

    update tf

--- day3/day3-storage-demo.tf
+++ day3/day3-storage-demo.tf
@@ -1,11 +1,22 @@
 # variable section 
 variable "az_subs" {
-  
-  type = string
-  default = "ee075321-f9dd-42f2-a56a-2f0a5141d191"
+
+  type        = string
+  default     = "ee075321-f9dd-42f2-a56a-2f0a5141d191"
   description = "this is my azure account subs id"
 }
 
+variable "az_storage_account" {
+  type    = string
+  default = "ashutoshhstacc"
+
+}
+
+variable "staccount_tier" {
+  type    = string
+  default = "Standard"
+
+}
 
 
 
@@ -28,6 +39,17 @@ provider "azurerm" {
 }
 
 # data source section
-data "azurerm_resource_group" "name" {
+data "azurerm_resource_group" "example" {
   name = "bluecross-group"
+}
+
+# storage account section
+resource "azurerm_storage_account" "example" {
+  name                     = var.az_storage_account
+  resource_group_name      = data.azurerm_resource_group.example.name
+  location                 = data.azurerm_resource_group.example.location
+  account_tier             = var.staccount_tier
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+
 }
